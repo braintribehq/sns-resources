a7aeb460649069b4edad6c531a2003fd1db0c83d
Matus Svrcek <matussvrcek@braintribe.com>
2021-07-06 01:04:48 +0200

    Fix bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bb1-tfstates"
+    bucket = "bt-tfstates"
     
     # override the key using -backend-config during init, e.g. terraform init -backend-config="key=tfstates/my_cloud_name"
     # terraform does not support variable here, see https://github.com/hashicorp/terraform/issues/13022
